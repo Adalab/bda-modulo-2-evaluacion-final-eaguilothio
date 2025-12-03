dfaa80b23443d140f44d96c332ed3152d3b76149
eaguilothio <psic.aguilothio@gmail.com>
2025-12-03 19:22:33 +0100

    cambios ejercicios SQL

--- modulo_2_evaluacion_final_eaguilo_ejercicio_2.sql
+++ modulo_2_evaluacion_final_eaguilo_ejercicio_2.sql
@@ -9,8 +9,8 @@ USE sakila; -- selecciona la base de datos
 SELECT DISTINCT title AS titulo_pelicula
 FROM film;
 
--- DISTINCT: muestra cada valor solo una vez, evitando duplicados.
--- AS: pone un nombre más fácil de entender a la columna que aparece en el resultado.
+-- DISTINCT: Muestra cada valor solo una vez, evitando duplicados.
+-- AS: Asigna un nombre más claro y comprensible a la columna que se muestra en el resultado.
 
 -- Ejercicio 2. Muestra los nombres de todas las peliculas que tengan una clasificacion de "PG-13".
 SELECT title AS titulo_pelicula, rating AS clasificacion
@@ -28,7 +28,7 @@ SELECT title AS titulo_pelicula, description AS descripcion_pelicula
 FROM film
 WHERE description LIKE '%amazing%'; 
 
--- LIKE: busca si una palabra o un patrón aparece en un texto.
+-- LIKE: Busca si una palabra o un patrón aparece en un texto.
 -- %palabra%: Busca todos los registros donde la palabra aparezca en cualquier posición.
 
 -- Ejercicio 4. Encuentra el titulo de todas las peliculas que tengan una duracion mayor a 120 minutos.
@@ -55,14 +55,14 @@ SELECT actor_id AS id_actor, first_name AS nombre_actor, last_name AS apellido_a
 FROM actor
 WHERE actor_id BETWEEN 10 and 20; 
 
--- BETWEEN: selecciona valores dentro de un rango, incluyendo los extremos.
+-- BETWEEN: Selecciona valores dentro de un rango, incluyendo los extremos.
 
 -- Ejercicio 8. Encuentra el titulo de las peliculas en la tabla film que no sean ni "R" ni "PG-13" en cuanto a su clasificacion.
 SELECT title AS titulo_pelicula, rating AS clasificacion
 FROM film
 WHERE rating NOT IN ("PG-13","R"); 
 
--- NOT IN: excluye valores específicos ("PG-13" y "R"). 
+-- NOT IN: Excluye valores específicos ("PG-13" y "R"). 
 -- VERSION LARGA: rating NOT IN ("PG-13") AND rating NOT IN ("R"); AND: combina condiciones; ambas deben cumplirse.
 
 -- Ejercicio 9. Encuentra la cantidad total de peliculas en cada clasificacion de la tabla film y muestra la clasificacion junto con el recuento.
@@ -71,9 +71,9 @@ FROM film
 GROUP BY rating
 ORDER BY COUNT(film_id) DESC;
 
--- COUNT: funcion agregada. Cuenta cuantas filas hay en un conjunto de datos.
--- GROUP BY: agrupa resultados según la columna usada con la funcion agregada.
--- ORDER BY: ordena de mayor a menor. Ayuda a la visualizacion.
+-- COUNT: Funcion agregada. Cuenta cuantas filas hay en un conjunto de datos.
+-- GROUP BY: Agrupa resultados según la columna usada con la funcion agregada.
+-- ORDER BY: Ordena de mayor a menor. Ayuda a la visualizacion.
 
 -- Ejercicio 10. Encuentra la cantidad total de peliculas alquiladas por cada cliente y muestra el ID del cliente, su nombre y apellido junto con la cantidad de peliculas alquiladas.
 SELECT c.customer_id AS id_cliente , c.first_name AS nombre_cliente, c.last_name AS apellido_cliente, COUNT(r.rental_id) AS total_peliculas_alquiladas
@@ -82,7 +82,7 @@ LEFT JOIN rental r ON c.customer_id = r.customer_id
 GROUP BY c.customer_id, c.first_name, c.last_name
 ORDER BY COUNT(r.rental_id) DESC;
 
--- LEFT JOIN: combina los datos de customer y rental, mostrando todos los clientes, incluso si no han alquilado películas (en ese caso, COUNT será 0).
+-- LEFT JOIN: Combina los datos de customer y rental, mostrando todos los clientes, incluso si no han alquilado películas (en ese caso, COUNT será 0).
 
 -- Ejercicio 11. Encuentra la cantidad total de peliculas alquiladas por categoria y muestra el nombre de la categoría junto con el recuento de alquileres.
 
